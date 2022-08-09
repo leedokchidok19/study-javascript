ff6a0e06e62b459568c188fa028896f234824931
rom <47617159+leedokchidok19@users.noreply.github.com>
2022-08-09 23:32:15 +0900

    게임:슈팅게임세팅 이미지 추가와 화면 render

--- webGame/shooting/main.js
+++ webGame/shooting/main.js
@@ -1,6 +1,9 @@
 /*
 이미지는 icon8 사이트에서 참고
 https://icons8.com/icons
+* bulletImage - https://icons8.com/icons/set/bullet
+* enemyImage - https://icons8.com/icons/set/enemy
+* gameOverImage - https://toppng.com/show_download/101861/game-over/large
 */
 //캔버스 세팅
 let canvas;
@@ -16,6 +19,10 @@ document.body.appendChild(canvas);
 
 let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
 
+//우주선 좌표(우주선의 크기는 60)
+let spaceshipX = canvas.width/2 - 60/2;
+let spaceshipY = canvas.height - 60;
+
 function loadImage() {
 
     backgroundImage = new Image();
@@ -25,12 +32,29 @@ function loadImage() {
     spaceshipImage.src = 'images/icons8-space-60.png';
 
     bulletImage = new Image();
-    bulletImage.src = 'images/hd-wallpaper-1867616_1920.jpg';
+    bulletImage.src = 'images/icons8-bullet-50.png';
 
     enemyImage = new Image();
-    enemyImage.src = 'images/hd-wallpaper-1867616_1920.jpg';
+    enemyImage.src = 'images/icons8-air-67.png';
 
     gameOverImage = new Image();
-    gameOverImage.src = 'images/hd-wallpaper-1867616_1920.jpg';
+    gameOverImage.src = 'images/toppng.com-game-over-870x630.png';
 
 }
+
+function render() {
+
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+
+}
+
+function main() {
+    
+    render();
+    //console.log('animation calls main function');
+    requestAnimationFrame(main);
+}
+
+loadImage();
+main();
