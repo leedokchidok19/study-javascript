d691242310e5ddf8b1e8b91e8ffb517be917ccc9
leedokchidok19 <duckhyun4433@naver.com>
2022-08-27 21:19:40 +0900

    study js

---
+++ ES6/index.js
@@ -0,0 +1,14 @@
+/*
+* cmd > node 입력하면 브라우저에서처럼 js 사용 가능
+    + node
+* js 파일 실행 방법
+    + cmd > node 파일명
+    + node 파일명
+* nodemon 설치
+    + npm install nodemon -g
+
+> node 파일명과 nodemon과 차이
+> node 파일명은 한 번만 실행
+> nodemon으로 실행하면 수정한 내용 반영
+*/
+console.log('Hellow World!');
